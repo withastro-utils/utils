7c8e37f377840e9324585f21a8db9760fb9b6015
ido <ido.pluto@w-on.uk>
2024-04-01 22:50:52 +0300

    fix(select): lock context

--- packages/context/src/index.ts
+++ packages/context/src/index.ts
@@ -1,24 +1,35 @@
-import {AstroGlobal} from 'astro';
+import { AstroGlobal } from 'astro';
 
 declare global {
     export namespace App {
         export interface Locals {
-            [key: string]: any
+            [key: string]: any;
         }
     }
 }
 
-type ContextAstro = AstroGlobal | {request: Request, locals: any, props: any}
-function getContextHistory(astro: ContextAstro , name: string) {
-    const contexts = astro.locals.amContext ??= {};
-    return contexts[name] ??= [];
+type ContextAstro = AstroGlobal | { request: Request, locals: any, props: any; };
+function getContextHistory(astro: ContextAstro, name: string) {
+    const contexts: Map<string, any[]> = astro.locals.amContext ??= new Map();
+    contexts.set(name, contexts.get(name) ?? []);
+    return contexts.get(name);
 }
 
 export default function getContext(astro: ContextAstro, name = "default") {
     return getContextHistory(astro, name).at(-1) ?? {};
 }
 
-export async function asyncContext<T>(promise: () => Promise<T>, astro: ContextAstro, {name = "default", context = null} = {}): Promise<T> {
+type AsyncContextOptions = { name?: string, context?: any, lock?: string; };
+const activeLock: Map<string, Map<string, Promise<void>>> = new Map();
+
+export async function asyncContext<T>(promise: () => Promise<T>, astro: ContextAstro, { name = "default", context = null, lock }: AsyncContextOptions = {}): Promise<T> {
+    activeLock.set(name, activeLock.get(name) ?? new Map());
+    const lockContext = activeLock.get(name);
+
+    while (lockContext.get(lock)) {
+        await lockContext.get(lock);
+    }
+
     const contextHistory = getContextHistory(astro, name);
 
     contextHistory.push({
@@ -26,8 +37,20 @@ export async function asyncContext<T>(promise: () => Promise<T>, astro: ContextA
         ...contextHistory.at(-1)
     });
 
-    const response = await promise();
+    let resolver: () => void | null;
+    if (lock) {
+        lockContext.set(lock, new Promise<void>(resolve => resolver = resolve));
+    }
 
-    contextHistory.pop();
-    return response;
+    try {
+        const response = await promise();
+        contextHistory.pop();
+        return response;
+    } finally {
+        lockContext.delete(lock);
+        if(lockContext.size === 0) {
+            activeLock.delete(name);
+        }
+        resolver?.();
+    }
 }
